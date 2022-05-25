ac07158ee129f3b659bb8d089057d6e0fca53035
rdxdkr <franchi.davide95@gmail.com>
2022-05-25 17:45:53 +0200

    test subscribe_returns_a_200_for_valid_form_data red

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -20,6 +20,26 @@ mod tests {
         assert_eq!(Some(0), response.content_length());
     }
 
+    #[tokio::test]
+    async fn subscribe_returns_a_200_for_valid_form_data() {
+        // arrange
+        let address = spawn_app();
+        let client = reqwest::Client::new();
+
+        // act
+        let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+        let response = client
+            .post(format!("{}/subscriptions", address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        // assert
+        assert_eq!(200, response.status().as_u16());
+    }
+
     fn spawn_app() -> String {
         let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
         let port = listener.local_addr().unwrap().port();
