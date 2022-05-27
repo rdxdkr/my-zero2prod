8052ec306294d9aad2f1f598e8b190dd1105c6ad
rdxdkr <franchi.davide95@gmail.com>
2022-05-27 21:33:02 +0200

    hid db secrets to prevent accidental logging

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -6,6 +6,7 @@ mod tests {
         telemetry::{get_subscriber, init_subscriber},
     };
     use once_cell::sync::Lazy;
+    use secrecy::ExposeSecret;
     use sqlx::{Connection, Executor, PgConnection, PgPool};
     use std::net::TcpListener;
     use uuid::Uuid;
@@ -130,9 +131,10 @@ mod tests {
 
     async fn configure_database(config: &DatabaseSettings) -> PgPool {
         // create db
-        let mut connection = PgConnection::connect(&config.connection_string_without_db())
-            .await
-            .expect("Failed to connect to Postgres");
+        let mut connection =
+            PgConnection::connect(&config.connection_string_without_db().expose_secret())
+                .await
+                .expect("Failed to connect to Postgres");
 
         connection
             .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
@@ -140,7 +142,7 @@ mod tests {
             .expect("Failed to create database.");
 
         // migrate db
-        let connection_pool = PgPool::connect(&config.connection_string())
+        let connection_pool = PgPool::connect(&config.connection_string().expose_secret())
             .await
             .expect("Failed to connect to Postgres.");
 
