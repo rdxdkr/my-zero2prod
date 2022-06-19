51e613f25335f79c541519fc3b22237b0d93aad4
rdxdkr <franchi.davide95@gmail.com>
2022-06-19 12:18:57 +0200

    refactored settings to return connection options instead of raw connection string

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,6 +1,7 @@
 use config::ConfigError;
 use secrecy::{ExposeSecret, Secret};
 use serde_aux::field_attributes::deserialize_number_from_string;
+use sqlx::postgres::PgConnectOptions;
 use std::str::FromStr;
 
 #[derive(serde::Deserialize)]
@@ -27,25 +28,16 @@ pub struct DatabaseSettings {
 }
 
 impl DatabaseSettings {
-    pub fn connection_string(&self) -> Secret<String> {
-        Secret::new(format!(
-            "postgres://{}:{}@{}:{}/{}",
-            self.username,
-            self.password.expose_secret(),
-            self.host,
-            self.port,
-            self.database_name
-        ))
+    pub fn with_db(&self) -> PgConnectOptions {
+        self.without_db().database(&self.database_name)
     }
 
-    pub fn connection_string_without_db(&self) -> Secret<String> {
-        Secret::new(format!(
-            "postgres://{}:{}@{}:{}",
-            self.username,
-            self.password.expose_secret(),
-            self.host,
-            self.port
-        ))
+    pub fn without_db(&self) -> PgConnectOptions {
+        PgConnectOptions::new()
+            .host(&self.host)
+            .username(&self.username)
+            .password(&self.password.expose_secret())
+            .port(self.port)
     }
 }
 
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -6,7 +6,6 @@ mod tests {
         telemetry::{get_subscriber, init_subscriber},
     };
     use once_cell::sync::Lazy;
-    use secrecy::ExposeSecret;
     use sqlx::{Connection, Executor, PgConnection, PgPool};
     use std::net::TcpListener;
     use uuid::Uuid;
@@ -131,10 +130,9 @@ mod tests {
 
     async fn configure_database(config: &DatabaseSettings) -> PgPool {
         // create db
-        let mut connection =
-            PgConnection::connect(&config.connection_string_without_db().expose_secret())
-                .await
-                .expect("Failed to connect to Postgres");
+        let mut connection = PgConnection::connect_with(&config.without_db())
+            .await
+            .expect("Failed to connect to Postgres");
 
         connection
             .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
@@ -142,7 +140,7 @@ mod tests {
             .expect("Failed to create database.");
 
         // migrate db
-        let connection_pool = PgPool::connect(&config.connection_string().expose_secret())
+        let connection_pool = PgPool::connect_with(config.with_db())
             .await
             .expect("Failed to connect to Postgres.");
 
