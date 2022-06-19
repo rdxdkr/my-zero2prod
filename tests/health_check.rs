74e5ae048bf27ec02b8f2976518a1831a47ca766
rdxdkr <franchi.davide95@gmail.com>
2022-06-19 15:40:36 +0200

    test subscribe_returns_a_200_when_fields_are_present_but_empty green, but it shouldn't pass

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -76,6 +76,38 @@ mod tests {
         assert_eq!(saved.name, "le guin");
     }
 
+    // SHOULD NOT PASS
+    #[tokio::test]
+    async fn subscribe_returns_a_200_when_fields_are_present_but_empty() {
+        // arrange
+        let app = spawn_app().await;
+        let client = reqwest::Client::new();
+        let test_cases = vec![
+            ("name=&email=ursula_le_guin%40gmail.com", "empty name"),
+            ("name=Ursula&email=", "empty email"),
+            ("name=Ursula&email=definitely-not-an-email", "invalid email"),
+        ];
+        
+        for (body, description) in test_cases {
+            // act
+            let response = client
+                .post(&format!("{}/subscriptions", &app.address))
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .body(body)
+                .send()
+                .await
+                .expect("Failed to execute request.");
+
+            // assert
+            assert_eq!(
+                200,
+                response.status().as_u16(),
+                "The API did not return a 200 OK when the payload was {}.",
+                description
+            );
+        }
+    }
+
     #[tokio::test]
     async fn subscribe_returns_a_400_when_data_is_missing() {
         // arrange
