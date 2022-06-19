2c8d6088456c4aef97853c32ec68eab845c35e82
rdxdkr <franchi.davide95@gmail.com>
2022-06-19 13:42:31 +0200

    added check to use secure connection if available

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,7 +1,7 @@
 use config::ConfigError;
 use secrecy::{ExposeSecret, Secret};
 use serde_aux::field_attributes::deserialize_number_from_string;
-use sqlx::postgres::PgConnectOptions;
+use sqlx::postgres::{PgConnectOptions, PgSslMode};
 use std::str::FromStr;
 
 #[derive(serde::Deserialize)]
@@ -25,6 +25,7 @@ pub struct DatabaseSettings {
     pub port: u16,
     pub host: String,
     pub database_name: String,
+    pub require_ssl: bool,
 }
 
 impl DatabaseSettings {
@@ -33,11 +34,18 @@ impl DatabaseSettings {
     }
 
     pub fn without_db(&self) -> PgConnectOptions {
+        let ssl_mode = if self.require_ssl {
+            PgSslMode::Require
+        } else {
+            PgSslMode::Prefer
+        };
+
         PgConnectOptions::new()
             .host(&self.host)
             .username(&self.username)
             .password(&self.password.expose_secret())
             .port(self.port)
+            .ssl_mode(ssl_mode)
     }
 }
 
