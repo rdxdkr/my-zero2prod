3c9e64762cffac901544017a999e28875e550200
rdxdkr <franchi.davide95@gmail.com>
2022-06-22 00:15:12 +0200

    test send_email_succeeds_if_the_server_returns_200 green

--- src/email_client.rs
+++ src/email_client.rs
@@ -67,6 +67,7 @@ impl EmailClient {
 mod tests {
     use super::EmailClient;
     use crate::domain::SubscriberEmail;
+    use claim::assert_ok;
     use fake::{
         faker::{
             internet::en::SafeEmail,
@@ -76,7 +77,7 @@ mod tests {
     };
     use secrecy::Secret;
     use wiremock::{
-        matchers::{header, header_exists, method, path},
+        matchers::{any, header, header_exists, method, path},
         Mock, MockServer, Request, ResponseTemplate,
     };
 
@@ -126,5 +127,35 @@ mod tests {
 
         // assert
         // MockServer verifies expectations at the end of its scope
+    }
+
+    #[tokio::test]
+    async fn send_email_succeeds_if_the_server_returns_200() {
+        // arrange
+        let mock_server = MockServer::start().await;
+        let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let email_client = EmailClient::new(mock_server.uri(), sender, Secret::new(Faker.fake()));
+        let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let subject: String = Sentence(1..2).fake();
+        let content: String = Paragraph(1..10).fake();
+
+        // We do not copy in all the matchers we have in the other test.
+        // The purpose of this test is not to assert on the request we
+        // are sending out!
+        // We add the bare minimum needed to trigger the path we want
+        // to test in `send_email`.
+        Mock::given(any())
+            .respond_with(ResponseTemplate::new(200))
+            .expect(1)
+            .mount(&mock_server)
+            .await;
+
+        // act
+        let outcome = email_client
+            .send_email(subscriber_email, &subject, &content, &content)
+            .await;
+            
+        // assert
+        assert_ok!(outcome);
     }
 }
