0a6f05ed57f523b5868c178ed62cceb3c73ddd5a
rdxdkr <franchi.davide95@gmail.com>
2022-06-22 22:53:22 +0200

    test confirmations_without_token_are_rejected_with_a_400 green

--- src/startup.rs
+++ src/startup.rs
@@ -1,7 +1,7 @@
 use crate::{
     configuration::{DatabaseSettings, Settings},
     email_client::EmailClient,
-    routes::{health_check, subscribe},
+    routes::{confirm, health_check, subscribe},
 };
 use actix_web::{
     dev::Server,
@@ -69,6 +69,7 @@ pub fn run(
             .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .route("/subscriptions/confirm", web::get().to(confirm))
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
     })
