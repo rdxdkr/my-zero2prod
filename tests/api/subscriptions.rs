517a84c837c1b32169d72fcde449c951d7a007bd
rdxdkr <franchi.davide95@gmail.com>
2022-06-22 21:08:03 +0200

    test subscribe_sends_a_confirmation_email_for_valid_data red

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -1,4 +1,8 @@
 use crate::helpers::spawn_app;
+use wiremock::{
+    matchers::{method, path},
+    Mock, ResponseTemplate,
+};
 
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
@@ -67,4 +71,24 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
             error_message
         );
     }
+}
+
+#[tokio::test]
+async fn subscribe_sends_a_confirmation_email_for_valid_data() {
+    // arrange
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    // act
+    app.post_subscriptions(body.into()).await;
+
+    // assert
+    // Mock verifies expectations at the end of its scope
 }
