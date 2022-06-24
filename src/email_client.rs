35733ec4a93712b05527d51454a3b641370716f4
rdxdkr <franchi.davide95@gmail.com>
2022-06-24 20:51:34 +0200

    test newsletters_are_delivered_to_confirmed_subscribers green

--- src/email_client.rs
+++ src/email_client.rs
@@ -39,7 +39,7 @@ impl EmailClient {
 
     pub async fn send_email(
         &self,
-        recipient: SubscriberEmail,
+        recipient: &SubscriberEmail,
         subject: &str,
         html_content: &str,
         text_content: &str,
@@ -121,7 +121,7 @@ mod tests {
 
         // act
         let _ = email_client
-            .send_email(email(), &subject(), &content(), &content())
+            .send_email(&email(), &subject(), &content(), &content())
             .await;
 
         // assert
@@ -142,7 +142,7 @@ mod tests {
 
         // act
         let outcome = email_client
-            .send_email(email(), &subject(), &content(), &content())
+            .send_email(&email(), &subject(), &content(), &content())
             .await;
 
         // assert
@@ -163,7 +163,7 @@ mod tests {
 
         // act
         let outcome = email_client
-            .send_email(email(), &subject(), &content(), &content())
+            .send_email(&email(), &subject(), &content(), &content())
             .await;
 
         // assert
@@ -185,7 +185,7 @@ mod tests {
 
         // act
         let outcome = email_client
-            .send_email(email(), &subject(), &content(), &content())
+            .send_email(&email(), &subject(), &content(), &content())
             .await;
 
         // assert
