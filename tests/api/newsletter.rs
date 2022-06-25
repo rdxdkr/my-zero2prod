e5860b5d3f2eead39993448d5d20afdf887f496d
rdxdkr <franchi.davide95@gmail.com>
2022-06-25 21:37:46 +0200

    test invalid_password_is_rejected green

--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -146,6 +146,37 @@ async fn non_existing_user_is_rejected() {
     );
 }
 
+#[tokio::test]
+async fn invalid_password_is_rejected() {
+    // arrange
+    let app = spawn_app().await;
+    let username = &app.test_user.username;
+    let password = Uuid::new_v4().to_string();
+
+    assert_ne!(app.test_user.password, password);
+
+    let response = reqwest::Client::new()
+        .post(&format!("{}/newsletters", &app.address))
+        .basic_auth(username, Some(password))
+        .json(&serde_json::json!({
+            "title": "Newsletter title",
+            "content": {
+                "text": "Newsletter body as plain text",
+                "html": "<p>Newsletter body as HTML</p>",
+            }
+        }))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // assert
+    assert_eq!(401, response.status().as_u16());
+    assert_eq!(
+        r#"Basic realm="publish""#,
+        response.headers()["WWW-Authenticate"]
+    );
+}
+
 async fn create_unconfirmed_subscriber(app: &TestApp) -> ConfirmationLinks {
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
     let _mock_guard = Mock::given(path("/email"))
