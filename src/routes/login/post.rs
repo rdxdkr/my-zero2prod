0f605a811799139c939202352189803d8b7d9330
rdxdkr <franchi.davide95@gmail.com>
2022-06-26 15:12:35 +0200

    reenabled error reporting

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,5 +1,6 @@
 use crate::authentication::{validate_credentials, AuthError, Credentials};
 use crate::routes::error_chain_fmt;
+use actix_web::error::InternalError;
 use actix_web::http::header::LOCATION;
 use actix_web::{web, HttpResponse};
 use hmac::{Hmac, Mac};
@@ -35,7 +36,7 @@ pub async fn login(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
     secret: web::Data<Secret<String>>,
-) -> HttpResponse {
+) -> Result<HttpResponse, InternalError<LoginError>> {
     let credentials = Credentials {
         username: form.0.username,
         password: form.0.password,
@@ -44,9 +45,9 @@ pub async fn login(
     match validate_credentials(credentials, &pool).await {
         Ok(user_id) => {
             tracing::Span::current().record("user_id", &tracing::field::display(&user_id));
-            HttpResponse::SeeOther()
+            Ok(HttpResponse::SeeOther()
                 .insert_header((LOCATION, "/"))
-                .finish()
+                .finish())
         }
         Err(e) => {
             let e = match e {
@@ -61,13 +62,14 @@ pub async fn login(
                 mac.update(query_string.as_bytes());
                 mac.finalize().into_bytes()
             };
-
-            HttpResponse::SeeOther()
+            let response = HttpResponse::SeeOther()
                 .insert_header((
                     LOCATION,
                     format!("/login?{}&tag={:x}", query_string, hmac_tag),
                 ))
-                .finish()
+                .finish();
+
+            Err(InternalError::from_response(e, response))
         }
     }
 }
