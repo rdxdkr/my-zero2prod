d3520a5832177c4c39997ed28454632c470db3ed
rdxdkr <franchi.davide95@gmail.com>
2022-06-28 17:36:23 +0200

    handled cookies with reqwest feature

--- tests/api/login.rs
+++ tests/api/login.rs
@@ -1,6 +1,4 @@
 use crate::helpers::{assert_is_redirect_to, spawn_app};
-use reqwest::header::HeaderValue;
-use std::collections::HashSet;
 
 #[tokio::test]
 async fn an_error_flash_message_is_set_on_failure() {
@@ -13,13 +11,9 @@ async fn an_error_flash_message_is_set_on_failure() {
         "password": "random-password"
     });
     let response = app.post_login(&login_body).await;
-    let cookies: HashSet<_> = response
-        .headers()
-        .get_all("Set-Cookie")
-        .into_iter()
-        .collect();
+    let flash_cookie = response.cookies().find(|c| c.name() == "_flash").unwrap();
 
     // assert
     assert_is_redirect_to(&response, "/login");
-    assert!(cookies.contains(&HeaderValue::from_str("_flash=Authentication failed").unwrap()));
+    assert_eq!(flash_cookie.value(), "Authentication failed");
 }
