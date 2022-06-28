afcec9d460fd7c4884a0490e6b85db1d047570ce
rdxdkr <franchi.davide95@gmail.com>
2022-06-29 00:56:13 +0200

    test redirect_to_admin_dashboard_after_login_success red

--- tests/api/login.rs
+++ tests/api/login.rs
@@ -5,14 +5,14 @@ async fn an_error_flash_message_is_set_on_failure() {
     // arrange
     let app = spawn_app().await;
 
-    // act
+    // act 1
     let login_body = serde_json::json!({
         "username": "random-username",
         "password": "random-password"
     });
     let response = app.post_login(&login_body).await;
 
-    // assert
+    // assert 1
     assert_is_redirect_to(&response, "/login");
 
     // act 2
@@ -26,4 +26,26 @@ async fn an_error_flash_message_is_set_on_failure() {
 
     // assert 3
     assert!(!html_page.contains("<p><i>Authentication failed</i></p>"));
+}
+
+#[tokio::test]
+async fn redirect_to_admin_dashboard_after_login_success() {
+    // arrange
+    let app = spawn_app().await;
+
+    // act 1
+    let login_body = serde_json::json!({
+        "username": &app.test_user.username,
+        "password": &app.test_user.password
+    });
+    let response = app.post_login(&login_body).await;
+
+    // assert 1
+    assert_is_redirect_to(&response, "/admin/dashboard");
+
+    // act 2
+    let html_page = app.get_admin_dashboard().await;
+
+    // assert 2
+    assert!(html_page.contains(&format!("Welcome {}", app.test_user.username)));
 }
