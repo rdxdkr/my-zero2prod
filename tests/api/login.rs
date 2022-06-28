25cfafe4ef0acd120d6514acd3d849ff4163a175
rdxdkr <franchi.davide95@gmail.com>
2022-06-28 17:34:26 +0200

    test an_error_flash_message_is_set_on_failure red

--- tests/api/login.rs
+++ tests/api/login.rs
@@ -1,4 +1,6 @@
 use crate::helpers::{assert_is_redirect_to, spawn_app};
+use reqwest::header::HeaderValue;
+use std::collections::HashSet;
 
 #[tokio::test]
 async fn an_error_flash_message_is_set_on_failure() {
@@ -11,7 +13,13 @@ async fn an_error_flash_message_is_set_on_failure() {
         "password": "random-password"
     });
     let response = app.post_login(&login_body).await;
+    let cookies: HashSet<_> = response
+        .headers()
+        .get_all("Set-Cookie")
+        .into_iter()
+        .collect();
 
     // assert
     assert_is_redirect_to(&response, "/login");
+    assert!(cookies.contains(&HeaderValue::from_str("_flash=Authentication failed").unwrap()));
 }
