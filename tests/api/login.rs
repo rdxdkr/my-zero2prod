82f003cd04ca0525707ede369d59bd819d3462ea
rdxdkr <franchi.davide95@gmail.com>
2022-06-28 17:28:15 +0200

    test an_error_flash_message_is_set_on_failure red

--- tests/api/login.rs
+++ tests/api/login.rs
@@ -1,4 +1,4 @@
-use crate::helpers::spawn_app;
+use crate::helpers::{assert_is_redirect_to, spawn_app};
 
 #[tokio::test]
 async fn an_error_flash_message_is_set_on_failure() {
@@ -13,5 +13,5 @@ async fn an_error_flash_message_is_set_on_failure() {
     let response = app.post_login(&login_body).await;
 
     // assert
-    assert_eq!(response.status().as_u16(), 303);
+    assert_is_redirect_to(&response, "/login");
 }
