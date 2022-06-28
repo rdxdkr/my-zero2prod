c3733b90924ff4a0b62ba957432de1753c8cbc1c
rdxdkr <franchi.davide95@gmail.com>
2022-06-28 23:00:24 +0200

    test an_error_flash_message_is_set_on_failure green

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,4 +1,8 @@
-use actix_web::{http::header::ContentType, HttpRequest, HttpResponse};
+use actix_web::{
+    cookie::{time::Duration, Cookie},
+    http::header::ContentType,
+    HttpRequest, HttpResponse,
+};
 
 pub async fn login_form(request: HttpRequest) -> HttpResponse {
     let error_html = match request.cookie("_flash") {
@@ -10,6 +14,7 @@ pub async fn login_form(request: HttpRequest) -> HttpResponse {
 
     HttpResponse::Ok()
         .content_type(ContentType::html())
+        .cookie(Cookie::build("_flash", "").max_age(Duration::ZERO).finish())
         .body(format!(
             r#"<!DOCTYPE html>
             <html lang="en">
