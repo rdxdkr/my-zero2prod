38e9cc2481e198db0493db6dfc877de02bd6813d
rdxdkr <franchi.davide95@gmail.com>
2022-06-29 18:15:18 +0200

    test current_password_must_be_valid red

--- tests/api/change_password.rs
+++ tests/api/change_password.rs
@@ -66,3 +66,36 @@ async fn new_password_fields_must_match() {
         "<p><i>You entered two different new passwords - the field values must match.</i></p>"
     ));
 }
+
+#[tokio::test]
+async fn current_password_must_be_valid() {
+    // arrange
+    let app = spawn_app().await;
+    let new_password = Uuid::new_v4().to_string();
+    let wrong_password = Uuid::new_v4().to_string();
+
+    // act 1
+    app.post_login(&serde_json::json!({
+        "username": &app.test_user.username,
+        "password": &app.test_user.password
+    }))
+    .await;
+
+    // act 2
+    let response = app
+        .post_change_password(&serde_json::json!({
+            "current_password": &wrong_password,
+            "new_password": &new_password,
+            "new_password_check": &new_password,
+        }))
+        .await;
+
+    // assert 2
+    assert_is_redirect_to(&response, "/admin/password");
+
+    // act 3
+    let html_page = app.get_change_password_html().await;
+
+    // assert 3
+    assert!(html_page.contains("<p><i>The current password is incorrect.</i></p>"));
+}
