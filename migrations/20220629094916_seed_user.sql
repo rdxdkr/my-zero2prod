6ffc2c3afd4955988a29f9addf6928b4a0c4eac1
rdxdkr <franchi.davide95@gmail.com>
2022-06-29 12:13:03 +0200

    enabled admin login with default password

---
+++ migrations/20220629094916_seed_user.sql
@@ -0,0 +1,8 @@
+INSERT INTO
+    users (user_id, username, password_hash)
+VALUES
+    (
+        'ddf8994f-d522-4659-8d02-c1d479057be6',
+        'admin',
+        '$argon2id$v=19$m=15000,t=2,p=1$mYsZR0D8iTXjUI0pkZANCQ$SLI6ZGdOVpvFkdkMx0R9H1XRv3f4xc46Fts89dzWAE4'
+    );
