7306df600c778ad5aabab0adbd1e1829667fd773
rdxdkr <franchi.davide95@gmail.com>
2022-06-29 13:55:48 +0200

    test you_must_be_logged_in_to_change_your_password green

--- tests/api/change_password.rs
+++ tests/api/change_password.rs
@@ -1,4 +1,5 @@
 use crate::helpers::{assert_is_redirect_to, spawn_app};
+use uuid::Uuid;
 
 #[tokio::test]
 async fn you_must_be_logged_in_to_see_the_change_password_form() {
@@ -7,6 +8,25 @@ async fn you_must_be_logged_in_to_see_the_change_password_form() {
 
     // act
     let response = app.get_change_password().await;
+
+    // assert
+    assert_is_redirect_to(&response, "/login");
+}
+
+#[tokio::test]
+async fn you_must_be_logged_in_to_change_your_password() {
+    // arrange
+    let app = spawn_app().await;
+    let new_password = Uuid::new_v4().to_string();
+
+    // act
+    let response = app
+        .post_change_password(&serde_json::json!({
+        "current_password": Uuid::new_v4().to_string(),
+        "new_password": &new_password,
+        "new_password_check": &new_password,
+        }))
+        .await;
 
     // assert
     assert_is_redirect_to(&response, "/login");
