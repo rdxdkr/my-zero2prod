903df2999ffe5cfcd88d1e50ef550f6900be9af2
rdxdkr <franchi.davide95@gmail.com>
2022-06-30 18:11:56 +0200

    test newsletter_creation_is_idempotent red

--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -99,6 +99,53 @@ async fn you_must_be_logged_in_to_publish_a_newsletter() {
     assert_is_redirect_to(&response, "/login");
 }
 
+#[tokio::test]
+async fn newsletter_creation_is_idempotent() {
+    // arrange
+    let app = spawn_app().await;
+
+    create_confirmed_subscriber(&app).await;
+    app.test_user.login(&app).await;
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    // act 1
+    let newsletter_request_body = serde_json::json!({
+        "title": "Newsletter title",
+        "text_content": "Newsletter body as plain text",
+        "html_content": "<p>Newsletter body as HTML</p>",
+        "idempotency_key": uuid::Uuid::new_v4().to_string()
+    });
+    let response = app.post_publish_newsletter(&newsletter_request_body).await;
+
+    // assert 1
+    assert_is_redirect_to(&response, "/admin/newsletters");
+
+    // act 2
+    let html_page = app.get_publish_newsletter_html().await;
+
+    // assert 2
+    assert!(html_page.contains("<p><i>The newsletter issue has been published!</i></p>"));
+
+    // act 3
+    let response = app.post_publish_newsletter(&newsletter_request_body).await;
+
+    // assert 3
+    assert_is_redirect_to(&response, "/admin/newsletters");
+
+    // act 4
+    let html_page = app.get_publish_newsletter_html().await;
+
+    // assert 4
+    assert!(html_page.contains("<p><i>The newsletter issue has been published!</i></p>"));
+
+    // Mock verifies assertions at the end of its scope
+}
+
 async fn create_unconfirmed_subscriber(app: &TestApp) -> ConfirmationLinks {
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
     let _mock_guard = Mock::given(path("/email"))
